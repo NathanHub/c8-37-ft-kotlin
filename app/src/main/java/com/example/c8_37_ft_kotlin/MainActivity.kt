3083344666ccdcc7e32dd29ddc79b599afbd594e
Nathan <nathannba2424@gmail.com>
2022-11-14 22:15:19 -0400

    First stage of the Ecommerce project

---
+++ app/src/main/java/com/example/c8_37_ft_kotlin/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.c8_37_ft_kotlin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
